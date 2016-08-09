2dd6901569ae5f8b544dc4634fbc5a75d6003d6a
Eric Sowell <eric.sowell@gmail.com>
2016-08-09 07:33:51 -0500

    added console log

--- Program.cs
+++ Program.cs
@@ -1,7 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace BuildUp
 {
@@ -9,6 +6,7 @@ namespace BuildUp
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
         }
     }
 }
